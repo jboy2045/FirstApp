65c175ac22cd4421f9ce352111ed670da69178fd
jboy2045 <calebereid@gmail.com>
2017-07-20 16:42:38 -0500

    first commit

---
+++ app/src/main/java/com/creid/firstapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.creid.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void getHelloWorldText (View view){
+        Log.i("info", "Button pressed");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
